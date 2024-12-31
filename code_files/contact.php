4547be8a554cb1353b62df95845a88c7f8cc3667
AlexanderOblego80 <89616946+Alks080900@users.noreply.github.com>
2024-12-30 19:22:17 -0500

    Update contact.php

--- code_files/contact.php
+++ code_files/contact.php
@@ -86,8 +86,8 @@ $mail->isSMTP();
 // Configure SMTP settings
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;          // Enable SMTP authentication
-$mail->Username = 'uphsgregistrar@gmail.com'; // SMTP username
-$mail->Password = 'djoqjencqofcvauj'; // SMTP password
+$mail->Username = ''; // SMTP username
+$mail->Password = ''; // SMTP password
 $mail->SMTPSecure = 'tls';       // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 587;               // TCP port to connect to
 
@@ -100,7 +100,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $emailSubject = $_POST['topic'];
     $emailBody = $_POST['message'];
 
-    $mail->setFrom('uphsgregistrar@gmail.com', 'UPHS-GMA || REGISTRAR');
+    $mail->setFrom('your_email', 'UPHS-GMA || REGISTRAR');
     $mail->addAddress($recipientEmail);     // Add a recipient
     $mail->Subject = $emailSubject;
     $mail->Body    = $emailBody;
